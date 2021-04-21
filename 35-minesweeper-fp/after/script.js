d581d76282a0520f6b8a0dba58909272c0e4d5d9
greentea524 <cpdawei@gmail.com>
2021-04-20 23:51:23 -0700

    add score, level, difficulty

--- 35-minesweeper-fp/after/script.js
+++ 35-minesweeper-fp/after/script.js
@@ -13,24 +13,67 @@ import {
 
 const BOARD_SIZE = 10
 const NUMBER_OF_MINES = 3
-
-let board = createBoard(
-  BOARD_SIZE,
-  getMinePositions(BOARD_SIZE, NUMBER_OF_MINES)
-)
 const boardElement = document.querySelector(".board")
-const minesLeftText = document.querySelector("[data-mine-count]")
+const gameStatus = document.querySelector(".gamestatus")
 const messageText = document.querySelector(".subtext")
+const newGameButton = document.querySelector(".newgame")
+const scoreBoard = document.querySelector(".scoreboard")
+const level = document.querySelector(".level")
+const odometer = document.querySelector(".odometer")
+
+let board = null;
+let difficulty = 0;
+let score = 0;
+let scoreAdded = false;
+
+newGameButton.addEventListener("click", e => {
+    newGame()
+})
+
+function newGame() {
+
+    scoreAdded = false;
+
+    var boardSizeChoice = BOARD_SIZE;
+    var numberOfMines = NUMBER_OF_MINES + difficulty;
+
+    displayMinesLeft();
+    refreshScore();
+    refreshLevel();
+
+    board = createBoard(
+      boardSizeChoice,
+      getMinePositions(boardSizeChoice, numberOfMines)
+    )
+
+    render();
+}
 
 function render() {
   boardElement.innerHTML = ""
-  checkGameEnd()
 
-  getTileElements().forEach(element => {
-    boardElement.append(element)
-  })
+  if(board){
+      checkGameEnd()
 
-  listMinesLeft()
+      getTileElements().forEach(element => {
+        boardElement.append(element)
+      })
+
+      listMinesLeft()
+  }
+
+}
+
+function refreshScore() {
+  scoreBoard.innerHTML = score;
+}
+
+function refreshLevel() {
+  level.innerHTML = difficulty;
+}
+
+function displayMinesLeft() {
+  messageText.innerHTML = "Mines Left: <span data-mine-count></span>"
 }
 
 function getTileElements() {
@@ -73,23 +116,37 @@ boardElement.style.setProperty("--size", BOARD_SIZE)
 render()
 
 function listMinesLeft() {
-  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount(board)
+    let minesLeftText = document.querySelector("[data-mine-count]")
+    if(minesLeftText !== null){
+        minesLeftText.innerHTML = NUMBER_OF_MINES + difficulty - markedTilesCount(board)
+    }
 }
 
 function checkGameEnd() {
   const win = checkWin(board)
   const lose = checkLose(board)
 
-  if (win || lose) {
-    boardElement.addEventListener("click", stopProp, { capture: true })
-    boardElement.addEventListener("contextmenu", stopProp, { capture: true })
-  }
+  // if (win || lose) {
+  //   boardElement.addEventListener("click", stopProp, { capture: true })
+  //   boardElement.addEventListener("contextmenu", stopProp, { capture: true })
+  // }
 
   if (win) {
-    messageText.textContent = "You Win"
+
+    if(!scoreAdded){
+        difficulty++;
+        let thisRoundScore = (difficulty * 1.5) * 1000;
+        score = score + Math.round(thisRoundScore);
+        scoreAdded = true;
+        refreshScore();
+        refreshLevel();
+        messageText.textContent = "You Win" + " +" + thisRoundScore;
+    }
+
   }
   if (lose) {
     messageText.textContent = "You Lose"
+    if(difficulty > 0) difficulty--;
     board.forEach(row => {
       row.forEach(tile => {
         if (tile.status === TILE_STATUSES.MARKED) board = markTile(board, tile)
